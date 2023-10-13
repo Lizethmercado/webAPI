514c1bf4f5db73954ff5b63baac095a302fb0d78
Lizethmercado <lizeth.mercadob@cecar.edu.co>
2023-10-13 17:41:06 -0500

    ..

--- js/consumirapipaises.js
+++ js/consumirapipaises.js
@@ -1,83 +1,93 @@
+var respuestaPaisesAPI;
+
+
 function poblarDatosPaises() {
-
-    var url = ' https://restcountries.com/v3.1/independent?status=true'
+    var url = 'https://restcountries.com/v3.1/independent?status=true'
     fetch(url)
-    .then(respuesta => respuesta.json())
-    .then(paises => {
+        .then(respuesta => respuesta.json())
+        .then(paises => {
 
-     //  crearListaPaises(paises);
-     adicionarDatosTablaPaises(paises);
+            respuestaPaisesAPI=paises;
 
-    });
+            // crearListaPaises(paises)
+            adicionarDatosTablaPaises(paises);
+            agregarElementosSelect(paises);
 
-    function adicionarDatosTablaPaises(paises){
+        });
 
-        var tabla = document.getElementById("tablaPaises");
-        for (const pais of paises) {g
+    function adicionarDatosTablaPaises(paises) {
+        var tabla = document.getElementById("tablaPaises")
 
-            var fila = tabla.insertRow(-1);
-            var columnaNombre = fila.insertCell(0);
-            var columnaCapital = fila.insertCell(1);
-            var columnaMoneda = fila.insertCell(2);
-            var columnaBandera = fila.insertCell(3);
-            var columnaPoblacion= fila.insertCell(4);
-
-
-            columnaNombre.innerHTML = pais.name.official;
-            columnaCapital.innerHTML = pais.capital[0];
-            columnaMoneda.innerHTML = pais.name.currencies;
-            columnaBandera.innerHTML = pais.region;
-            columnaPoblacion.innerHTML = pais.population;
-
-
-
-            
-        }
-
-        
-    }
-
-    function crearListaPaises(paises) {
-
-        
-    }
-
-   /* function adicionarDatosTablaPaises(paises){
-
-        var tabla = document.getElementById("tablaPaises");
         for (const pais of paises) {
+            var fila = tabla.insertRow(-1)
+            var columnaNombre = fila.insertCell(0)
+            var columnaCapital = fila.insertCell(1)
+            var columnaRegion = fila.insertCell(2)
 
-            var fila = tabla.insertRow(-1);
-            var columnaNombre = fila.insertCell(0);
-            var columnaCapital = fila.insertCell(1);
-            var columnaMoneda = fila.insertCell(2);
-            var columnaBandera = fila.insertCell(3);
-            var columnaPoblacion= fila.insertCell(2);
-
-
-            columnaNombre.innerHTML = pais.name.official;
-            columnaCapital.innerHTML = pais.capital[0];
-            columnaRegion.innerHTML = pais.name.official;
-
-
-            
+            columnaNombre.innerHTML = pais.name.official
+            columnaCapital.innerHTML = pais.capital[0]
+            columnaRegion.innerHTML = pais.region
         }
 
-        
     }
 
-    function crearListaPaises(paises) {
+    /*function adicionarDatosTablaPaises(paises) {
+        var tabla = document.getElementById("tablaPaisesBonificacion")
 
-        var ul = crearNodo('ul');
+        for (const pais of paises) {
+            var fila = tabla.insertRow(-1)
+            var columnaNombre = fila.insertCell(0)
+            var columnaCapitales = fila.insertCell(1)
+            var columnaMonedas = fila.insertCell(2)
+            var columnaBandera = fila.insertCell(3)
+            var columnaPoblacion = fila.insertCell(4)
+
+            columnaNombre.innerHTML = pais.name.official
+            columnaCapitales.innerHTML = pais.capital[0]
+            columnaMonedas.innerHTML = pais.currencies
+            columnaBandera.innerHTML = pais.region
+            columnaPoblacion.innerHTML = pais.population
+        }
+
+    }*/
+
+    function agregarElementosSelect(paises) {
+
+        var selectPaises = document.getElementById('selectPaises');
 
         for (const pais of paises) {
 
-            var li = crearNodoTexto('li', pais.name.common);
-            adicionarNodoContenedor(ul,li);
+            var option = crearNodoTexto('option', pais.name.official);
+            adicionarNodoContenedor(selectPaises, option);
+
             
         }
         
-        adicionarNodoBody(ul);
-    } */
+    }
+
+   
+
+}
+function mostrarDatosPaisSeleccionado(paisBuscar) {
+
+
+    var tabla = document.getElementById("tablaPaises")
+    tabla.remove();
+
+    for (const pais of respuestaPaisesAPI) {
+        
+        if(paisBuscar== pais.name.official){
+        var fila = tabla.insertRow(-1)
+        var columnaNombre = fila.insertCell(0)
+        var columnaCapital = fila.insertCell(1)
+        var columnaRegion = fila.insertCell(2)
+
+        columnaNombre.innerHTML = pais.name.official
+        columnaCapital.innerHTML = pais.capital[0]
+        columnaRegion.innerHTML = pais.region
+
+        }
+        
+    }
     
 }
